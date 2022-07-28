ae726f78797cc8e0472b98b7b0b09a3bd130abb5
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-07-28 20:18:31 +0530

    user model

--- crm/server.js
+++ crm/server.js
@@ -2,10 +2,27 @@
  * This shoudle be the starting point of the application
  */
 
-const dbConfig = require("./configs/server.config");
+const dbConfig = require("./configs/db.config");
 const express = require("express");
 const serverConfig = require("./configs/server.config");
 const app = express();
+const mongoose  = require("mongoose");
+
+/**
+ * I need to connec to the database
+*/
+
+mongoose.connect(dbConfig.DB_URL);
+
+const db = mongoose.connection ;
+
+db.on('error' ,() => {
+    console.log("Error while connecting to DB");
+});
+
+db.once("open", ()=>{
+    console.log("Connected to database");
+})
 
 
 app.listen(serverConfig.PORT , () => {
