319315bf6591a583296e66db106e48667ab0849e
Vishwa Mohan <vimohan@vimohan-mn2.linkedin.biz>
2022-07-28 19:40:00 +0530

    CRM _ Server code

---
+++ crm/server.js
@@ -0,0 +1,15 @@
+/**
+ * This shoudle be the starting point of the application
+ */
+
+const dbConfig = require("./configs/server.config");
+const express = require("express");
+const serverConfig = require("./configs/server.config");
+const app = express();
+
+
+app.listen(serverConfig.PORT , () => {
+    console.log("Server started on the port no : ",serverConfig.PORT );
+})
+
+
